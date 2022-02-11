ab7a6d323de4436f7bae5e2a681950329212f21e
langejay <jayclange@yahoo.com.au>
2022-02-11 16:13:00 +1030

    More Errors/observations added 389-393

--- samples/ViewModelsSamples/Lines/Straight/ViewModel.cs
+++ samples/ViewModelsSamples/Lines/Straight/ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using LiveChartsCore;
+using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
 using System.Collections.Generic;
 
@@ -8,9 +9,16 @@ namespace ViewModelsSamples.Lines.Straight
     {
         public List<ISeries> Series { get; set; } = new List<ISeries>
         {
-            new LineSeries<double>
+            new LineSeries<ObservablePoint>
             {
-                Values = new List<double> { -2, -1, 3, 5, 3, 4, 6 },
+                // Values = new List<double> { -2, -1, 3, 5, 3, 4, 6 },
+                Values = new List<ObservablePoint>{
+                    new ObservablePoint{ X = 100000, Y = 3.4},
+                    new ObservablePoint{ X = 110000, Y = 2.2},
+                    new ObservablePoint{ X = 120000, Y = 3.9},
+                    new ObservablePoint{ X = 130000, Y = 4.1},
+                    new ObservablePoint{ X = 150000, Y = 3.4},
+                },
                 Fill = null,
                 // use the line smoothness property to control the curve
                 // it goes from 0 to 1
